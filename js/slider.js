dc5e1687bd8e0876a16d195f2cbe7530b1cb4c94
Adam Gray <68389728+asg-images@users.noreply.github.com>
2023-07-21 15:02:39 +0100

    Added client slider

--- js/slider.js
+++ js/slider.js
@@ -1,42 +1,40 @@
-//PARTNER SLIDER 
-$(".partner-slider").slick({
-        slidesToShow: 2,
-        autoplay: true,
-        speed: 200, 
-        inifnite: true, 
-        arrows: false, 
-        mobileFirst: true,
-        responsive: [
-            {
-                breakpoint: 741,
-                settings: {
-                  slidesToShow: 4,
-                }
-            }, 
-            {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 6,
-                }
-            }
-        ]
-})
+const slideSettings = {
+  slidesToShow: 2,
+  autoplay: true,
+  speed: 200, 
+  inifnite: true, 
+  arrows: false, 
+  mobileFirst: true,
+  responsive: [
+      {
+          breakpoint: 741,
+          settings: {
+            slidesToShow: 3,
+          }
+      }, 
+      {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+          }
+      }
+  ]
+}
 
 //HERO SLIDER
 $(".hero-slide").slick({
     slidesToShow: 1, 
-    // autoplay: true,
+    autoplay: true,
+    autoplayspeed: 2000,
     infinite: true,
     dots: true,
     arrows: false,
     cssEase: 'linear'       
 })
 
+
+//PARTNER SLIDER 
+$(".partner-slider").slick(slideSettings);
+
 //CUSTOMER SLIDER
-$(".slick-container").slick({
-    slidesToShow: 8,
-    autoplay: true,
-    speed: 200, 
-    inifnite: true, 
-    arrows: false
-})
+$(".slick-container").slick(slideSettings);
